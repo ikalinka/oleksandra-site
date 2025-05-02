b30801ae8dbb16989500e2a7871fd7fe67288814
Ihor Kalinka <ihorkalinka@Ihors-MacBook-Pro.local>
2025-05-01 22:42:11 -0700

    Fix root page redirect

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true, // optional if you use server actions
-  },
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
